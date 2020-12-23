d19880861bf73e98f281cd584b271b649de5ae8a
demaiodev <fml1041@gmail.com>
2020-12-23 18:13:07 -0500

    add components

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="p-8">
+    <div class="bg-white rounded-lg shadow-xl">
+      <div class="p-8">
+        <div>
+          <!-- Insert your svg or image here -->
+        </div>
+        <div class="mt-8 text-center">
+          <h1 class="font-bold text-lg text-gray-700 mb-1">
+            Customer support 24/7
+          </h1>
+          <p class="text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <button
+            class="mt-6 bg-blue-500 hover:bg-blue-400 text-white rounded-full px-12 py-3 shadow-xl focus:outline-none"
+          >
+            Get started
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Card'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,42 @@
-<template>Cool Main Page Whoa</template>
+<template>
+  <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+    <div class="px-4 py-6 sm:px-0">
+      <div class="flex flex-col items-center text-left">
+        <div v-if="error">There was an error :/</div>
+        <div v-if="loading" class="loading-box">
+          <div><Spinner /></div>
+        </div>
+        <div
+          v-else
+          class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5"
+        >
+          <!-- <Monsters v-for="(monster, i) inallMonsters.data" :key="i" :src="monster" /> -->
+          <Card />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 <script>
+import Card from '../components/Card.vue'
+import Spinner from '../components/Spinner.vue'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: { Card, Spinner },
+  data() {
+    return {
+      error: false,
+      loading: false
+    }
+  }
 }
 </script>
-<style></style>
+<style scoped>
+.loading-box {
+  min-height: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
